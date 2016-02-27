25f1702a30284d5b6faa3c00102e7e5e508b8b90
Dmytro Yarmak <dmytroy@backbase.com>
2016-02-27 15:44:54 +0200

    Implement reading of matrix on file select

--- src/app.js
+++ src/app.js
@@ -5,10 +5,141 @@
         .module('hadar', [
             'ngMessages'
         ])
-        .controller('HadarController', HadarController);
+        .factory('fileReader', fileReaderFactory)
+        .factory('matrixReader', matrixReaderFactory)
+        .controller('HadarController', HadarController)
+        .controller('DyOnFileChangeController', DyOnFileChangeController)
+        .directive('dyOnFileChange', dyOnFileChangeDirective);
 
-    HadarController.$inject = [];
-    function HadarController(){
+    fileReaderFactory.$inject = ['$q'];
+    function fileReaderFactory ($q) {
+        var service = {
+            readText: readText
+        };
+
+        return service;
+
+        //////////
+
+        function readText (file) {
+            if (file) {
+                return $q(function(resolve, reject) {
+                    var fileReader = new FileReader();
+
+                    fileReader.addEventListener('load', function() {
+                        resolve(fileReader.result);
+                    });
+
+                    fileReader.addEventListener('error', function() {
+                        reject('Failed to read the file');
+                    });
+
+                    fileReader.readAsText(file);
+                });
+            } else {
+                return $q.reject('No file passed');
+            }
+        }
+    }
+
+    matrixReaderFactory.$inject = ['fileReader'];
+    function matrixReaderFactory (fileReader) {
+        var COMMENT_LINE_INDICATOR = '%';
+        var HEADER_REGEX = /^%%MatrixMarket matrix (\w+) (\w+) (\w+)$/;
+        var DENSE_META_REGEX = /^\s*(\d+)\s+(\d+)$/;
+        var SPARSE_META_REGEX = /^\s*(\d+)\s+(\d+)\s+(\d+)$/;
+
+        var MATRIX_TYPE_TO_META_REGEX_MAP = {
+            SPARSE: SPARSE_META_REGEX,
+            DENSE: DENSE_META_REGEX
+        };
+
+        var service = {
+            read: read
+        };
+
+        return service;
+
+        //////////
+
+        function read (file) {
+            return fileReader
+                .readText(file)
+                .then(_parseMatrixMarketFile);
+        }
+
+        function _parseMatrixMarketFile (text) {
+            var lines = text.split('\n');
+            var headerLine = lines[0];
+            var bodyLines = lines.slice(1).filter(_isNotCommentLine);
+            var metaLine = bodyLines[0];
+            var valueLines = bodyLines.slice(1);
+
+            var headerLineMatchResult = headerLine.match(HEADER_REGEX);
+            if (!headerLineMatchResult) {
+                throw new Error('Can\'t parse header line: ' + headerLine);
+            }
+
+
+            var matrixType = _getMatrixTypeFromHeaderLineMatchResult(headerLineMatchResult);
+            var valuesType = _getValueTypeFromHeaderLineMatchResult(headerLineMatchResult);
+            var symmetry = _getSymmetryFromHeaderLineMatchResult(headerLineMatchResult);
+
+            var metaLineMatchResult = metaLine.match(MATRIX_TYPE_TO_META_REGEX_MAP[matrixType]);
+            if (!metaLineMatchResult) {
+                throw new Error('Can\'t parse meta line: ' + metaLine);
+            }
+
+            var rows = parseInt(metaLineMatchResult[1], 10);
+            var columns = parseInt(metaLineMatchResult[1], 10);
+            var entities = parseInt(metaLineMatchResult[2], 10);
+            var values = valueLines.join('\n');
+
+            return {
+                matrixType: matrixType,
+                valuesType: valuesType,
+                symmetry: symmetry,
+                rows: rows,
+                columns: columns,
+                entities: entities,
+                values: values
+            };
+        }
+
+        function _isNotCommentLine (line) {
+            return line[0] !== COMMENT_LINE_INDICATOR;
+        }
+
+        function _getMatrixTypeFromHeaderLineMatchResult (headerLineMatchResult) {
+            var inputType = headerLineMatchResult[1];
+            switch(inputType) {
+                case 'coordinate': return 'SPARSE';
+                case 'array': return 'DENSE';
+                default: throw new Error('Unsopported input type: ' + inputType);
+            }
+        }
+
+        function _getValueTypeFromHeaderLineMatchResult (headerLineMatchResult) {
+            var valueType = headerLineMatchResult[2];
+            switch(valueType) {
+                case 'real': return 'REAL';
+                case 'integer': return 'INTEGER';
+                default: throw new Error('Unsopported value type: ' + valueType);
+            }
+        }
+
+        function _getSymmetryFromHeaderLineMatchResult (headerLineMatchResult) {
+            var symmetry = headerLineMatchResult[3];
+            switch(symmetry) {
+                case 'general': return 'GENERAL';
+                case 'symmetric': return 'SYMMETRIC';
+                default: throw new Error('Unsopported symmetry: ' + symmetry);
+            }
+        }
+    }
+
+    HadarController.$inject = ['$log', 'matrixReader'];
+    function HadarController($log, matrixReader){
         var vm = this;
 
         vm.method = null;
@@ -22,18 +153,26 @@
                 label: 'Метод Холецького'
             }
         ];
-        vm.matrixType = null;
         vm.availableMatrixTypes = [
             {
                 value: 'DENSE',
                 label: 'Щільна матриця'
             },
             {
-                value: 'BAND',
+                value: 'SPARSE',
                 label: 'Розріджена матриця'
             }
         ];
-        vm.matrixSymmetry = null;
+        vm.availableValuesTypes = [
+            {
+                value: 'REAL',
+                label: 'Дійсні'
+            },
+            {
+                value: 'INTEGER',
+                label: 'Цілі'
+            }
+        ];
         vm.availableMatrixSymmetry = [
             {
                 value: 'GENERAL',
@@ -45,15 +184,58 @@
             }
         ];
         vm.matrixInputSource = 'FILE';
-        vm.matrixRows = NaN;
-        vm.matrixColumns = NaN;
-        vm.matrixValues = '';
         vm.vectorInputSource = 'FILE';
-        vm.vectorRows = NaN;
-        vm.matrixValues = '';
+        vm.matrix = _createEmptyMatrix();
+        vm.vector = _createEmptyMatrix();
         vm.maxProcessesCount = navigator.hardwareConcurrency;
         vm.processesCount = vm.maxProcessesCount;
         vm.showCalculationTime = true;
         vm.maxCalculationTime = 30;
+        vm.onFileChange = onFileChange;
+
+        //////////
+
+        function onFileChange (fieldName, file) {
+            if (file) {
+                matrixReader
+                    .read(file)
+                    .then(function onReadMatrixSuccess(matrix) {
+                        vm[fieldName] = matrix;
+                    }, function onReadMatrixError (error) {
+                        $log.error(error);
+                    });
+            } else {
+                vm[fieldName] = _createEmptyMatrix();
+            }
+        }
+
+        function _createEmptyMatrix () {
+            return {
+                matrixType: null,
+                valuesType: null,
+                symmetry: null,
+                rows: null,
+                columns: null,
+                entities: null,
+                values: null
+            };
+        }
+    }
+
+    dyOnFileChangeDirective.$inject = [];
+    function dyOnFileChangeDirective () {
+        return {
+            restrict: 'A',
+            controller: 'DyOnFileChangeController'
+        };
+    }
+
+    DyOnFileChangeController.$inject = ['$element', '$scope', '$attrs'];
+    function DyOnFileChangeController ($element, $scope, $attrs) {
+        if ($element[0].tagName === 'INPUT' && $element[0].type === 'file') {
+            $element.on('change', function(event) {
+                $scope.$eval($attrs.dyOnFileChange, {$file: event.target.files[0]});
+            });
+        }
     }
 }());
